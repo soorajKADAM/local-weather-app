7986f0e4d499545aab42d787486fdcfc893dbb3c
soorajKADAM <86524347+soorajKADAM@users.noreply.github.com>
2023-04-01 17:19:54 +0530

    unit tests added

--- src/app/current-weather/current-weather.component.spec.ts
+++ src/app/current-weather/current-weather.component.spec.ts
@@ -1,23 +1,64 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { injectSpy } from 'angular-unit-test-helper';
+import { of } from 'rxjs';
 
+import { WeatherService } from '../weather/weather.service';
+import {fakeWeather} from '../weather/weather.service.fake';
 import { CurrentWeatherComponent } from './current-weather.component';
 
 describe('CurrentWeatherComponent', () => {
   let component: CurrentWeatherComponent;
   let fixture: ComponentFixture<CurrentWeatherComponent>;
+  let weatherServiceMock: jasmine.SpyObj<WeatherService>
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CurrentWeatherComponent ]
+  beforeEach(
+    waitForAsync(() => {
+      const weatherServiceSpy = jasmine.createSpyObj('WeatherService', [
+        'getCurrentWeather',
+      ])
+
+      TestBed.configureTestingModule({
+        declarations: [CurrentWeatherComponent],
+        // imports: [HttpClientTestingModule], // If we have to include HttpClientTestingModule then we're not really writing a unit test, because CurrentWeatherComponent shouldn't know about HttpClient
+        providers: [{ provide: WeatherService, useValue: weatherServiceSpy }],
+      }).compileComponents()
+
+      // weatherServiceMock = TestBed.inject(WeatherService) as any
+      weatherServiceMock = injectSpy(WeatherService)
     })
-    .compileComponents();
+  )
 
-    fixture = TestBed.createComponent(CurrentWeatherComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CurrentWeatherComponent)
+    component = fixture.componentInstance
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    // Arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of())
+
+    // Act
+    fixture.detectChanges() // triggers ngOnInit
+
+    // Assert
+    expect(component).toBeTruthy()
+  })
+  it('should eagerly load currentWeather in Bethesda from weatherService', () => {
+    // Arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of(fakeWeather))
+
+    // Act
+    fixture.detectChanges() // triggers ngOnInit()
+
+    // Assert
+    expect(component.current).toBeDefined()
+    expect(component.current.city).toEqual('Bethesda')
+    expect(component.current.temperature).toEqual(280.32)
+
+    // Assert on DOM
+    const debugEl = fixture.debugElement
+    const titleEl: HTMLElement = debugEl.query(By.css('span')).nativeElement
+    expect(titleEl.textContent).toContain('Bethesda')
+  })
 });
--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -20,10 +20,16 @@ interface ICurrentWeatherData {
   dt: number,
   name: string
 }
+export interface IWeatherService {
+  getCurrentWeather(
+  city: string,
+  country: string
+  ): Observable<ICurrentWeather>
+}
 @Injectable({
   providedIn: 'root'
 })
-export class WeatherService {
+export class WeatherService implements IWeatherService {
 
   constructor(private httpClient: HttpClient) { }
   getCurrentWeather(city: string, country: string):
