ddd419d615340a7556cda22a5eb9d184e3ccea6a
soorajKADAM <86524347+soorajKADAM@users.noreply.github.com>
2023-04-03 23:40:57 +0530

    UI enhanced with angular material

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 
 import { ICurrentWeather } from '../interfaces'
 import { WeatherService } from '../weather/weather.service'
@@ -8,7 +8,14 @@ import { WeatherService } from '../weather/weather.service'
   templateUrl: './current-weather.component.html',
   styleUrls: ['./current-weather.component.css'],
 })
-export class CurrentWeatherComponent {
+export class CurrentWeatherComponent implements OnInit {
+  getOrdinal(date: number) {
+    const n = new Date(date).getDate()
+    return n > 0
+      ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) ||
+                                  n % 10 > 3 ? 0 : n % 10]
+      : ''
+  }
   current: ICurrentWeather
   constructor(private weatherService: WeatherService) {}
   ngOnInit(): void {
